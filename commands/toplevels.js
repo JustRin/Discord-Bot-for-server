4ca5f518190e99525aadaa26b59865468731ecc5
JustRin <40486474+JustRin@users.noreply.github.com>
2019-01-19 15:32:12 +0300

    Update toplevels.js

--- commands/toplevels.js
+++ commands/toplevels.js
@@ -1,27 +1,38 @@
 const Discord = require("discord.js");
+const mysql = require("mysql");
 
 module.exports.run = async (bot, message, args) => {
-  let toplevels = new Discord.RichEmbed()
-  // 
-  // let xp = xp[message.author.id].xp;
-  // let lvl = xp[message.author.id].level;
-  // let time = Time[message.author.id].time;
-  // var max = Math.max.apply(null, xp);
+  var con = mysql.createConnection({
+	host: "sql2.freesqldatabase.com",
+	user: "sql2274820",
+	password: "rA7%zR3!",
+	database: "sql2274820"
+});
+    con.query(`SELECT * FROM rcbot ORDER BY time DESC`, (err, result, rows, fields) =>{
+        let toplevels = new Discord.RichEmbed()
+        .setDescription("**Top 10  :new_moon_with_face:**")
+        .setColor("#4d4dff");
 
-  .setDescription("**Top 10 пользователей на сервере 🌚**")
-  .setColor("#4d4dff")
-  .addField(`#1 ${Name}`, "**XP: ** `${max}` | **LVL:** `NaN` | **Время в войсе:** `NaN` ")
-  .addField(`#2 ${Name}`, "**XP:** `NaN` | **LVL:** `NaN` | **Время в войсе:** `NaN` ")
-  .addField(`#3 ${Name}`, "**XP:** `NaN` | **LVL:** `NaN` | **Время в войсе:** `NaN` ")
-  .addField(`#4 ${Name}`, "**XP:** `NaN` | **LVL:** `NaN` | **Время в войсе:** `NaN` ")
-  .addField(`#5 ${Name}`, "**XP:** `NaN` | **LVL:** `NaN` | **Время в войсе:** `NaN` ")
-  .addField(`#6 ${Name}`, "**XP:** `NaN` | **LVL:** `NaN` | **Время в войсе:** `NaN` ")
-  .addField(`#7 ${Name}`, "**XP:** `NaN` | **LVL:** `NaN` | **Время в войсе:** `NaN` ")
-  .addField(`#8 ${Name}`, "**XP:** `NaN` | **LVL:** `NaN` | **Время в войсе:** `NaN` ")
-  .addField(`#9 ${Name}`, "**XP:** `NaN` | **LVL:** `NaN` | **Время в войсе:** `NaN` ")
-  .addField(`#10 ${Name}`, "**XP:** `NaN` | **LVL:** `NaN` | **Время в войсе:** `NaN` ")
-  message.channel.send(toplevels);
+
+        for (let index = 0; index < 10; index++) {
+            if(result[index] !=null) {
+        let guildMember = bot.users.get(result[index])
+		let username = guildMember == null ? "Unnamed" : guildMember.username
+                 toplevels.addField(`#${index+1} <@${username}>`, `**XP: ** ${result[index].xp} | **LVL:** ${result[index].level} | **Time:** ${result[index].time} `);
+            }
+        }
+
+
+        toplevels.addField(`===========================================================`)
+        let pos = result.findIndex(obj => obj['did'] === message.author.id) + 1;
+        let data = result[pos - 1];
+		
+        toplevels.addField(`"Your position"#${pos} ${message.author.username}`, `**XP:** ${data.xp} | **LVL:** ${data.level} | **Time:** ${data.time} `)
+        message.channel.send(toplevels);
+    });
+  
 }
+
 
 module.exports.help = {
   name:"toplevels"
